1c1065bb543699a98bd0d8e0a2f4ccbf841083d1
stevecyj <ycsteven.tsao@gmail.com>
2021-09-08 14:00:57 +0800

    chore:update RegisterController, create method

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -72,6 +72,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'google2fa_secret' => $data['google2fa_secret'],
         ]);
     }
 
