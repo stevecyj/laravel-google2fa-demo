c4c8fe72939210e130a5f13323226519b285d7a4
stevecyj <ycsteven.tsao@gmail.com>
2021-09-08 14:02:07 +0800

    chore:Set up routes for complete-registeration

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/**
+ *complete-registeration
+ */
+Route::get('/complete-registration', [App\Http\Controllers\Auth\RegisterController::class, 'completeRegistration'])->name('complete-registration');
